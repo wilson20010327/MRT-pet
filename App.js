9f3a6fd70c21cd78df8bf963fea21e78d9b52dd3
wilson20010327 <wilson.l20010327@gmail.com>
2024-03-02 01:37:00 +0800

    Add bottom tab navigation
    Add two pages according to the figma, besides I have costimize the tab
    bar icon

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AssistantPage from './Assitant';
+import PetPage from './Pet';
+import TabBarIcon1 from './component/tab/TabBarIcon1';
+import TabBarIcon2 from './component/tab/TabBarIcon2';
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <Tab.Navigator >
+        <Tab.Screen name="AI" component={AssistantPage} options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ focused,color, size }) => (
+            <TabBarIcon1 focused={focused} />
+          ),
+        }}/>
+        <Tab.Screen name="Pet" component={PetPage} options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ focused,color, size }) => (
+            <TabBarIcon2 focused={focused}/>
+          ),
+        }}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ component/tab/TabBarIcon2.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+export default function TabBarIcon2(focused) {
+    return (
+        <View style={styles.container}>
+            <Image style={{
+                width: 40,
+                height: 40,
+                resizeMode: 'contain',
+                
+            }} source={ focused.focused?require('../../assets/aiIcon-click.png'):require('../../assets/aiIcon-unclick.png')} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop:20
+    },
+    
+});
