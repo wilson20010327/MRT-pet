521273b9425b887fb18af7538b313fc58fa0a325
wilson20010327 <wilson.l20010327@gmail.com>
2024-03-03 18:17:16 +0800

    Add the header component into the pet and ai page

--- Assitant.js
+++ Assitant.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Header from './component/Header-self';
 export default function AssistantPage() {
   return (
     <View style={styles.container}>
+      <Header/>
       <Text>This is the AI assistant page</Text>
       <StatusBar style="auto" />
     </View>
@@ -15,6 +16,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
--- Pet.js
+++ Pet.js
@@ -19,32 +19,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     // justifyContent: 'center',
   },
-  header: {
-    width:'100%',
-    height: 125,
-    // backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
-    resizeMode: 'contain',
-    flexDirection: 'row'
-  },
-  headerIcon_space:{
-    height:'100%',
-    width:'25%',
-    backgroundColor: '#000000',
-    alignItems: 'center',
-  },
-  center_space:{
-    height:'100%',
-    width:'50%',
-    backgroundColor: 'white',
-    alignItems: 'center',
-  },
-  content_space: {
-    width:'100%',
-    height: '100%',
-    backgroundColor: 'red',
-    alignItems: 'center',
-    // justifyContent: 'center',
-  },
 });
