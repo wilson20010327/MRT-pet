f927fbf7a913f7a233f9c8578fd65c57f5097ccc
HUAHSUAN-LIANG <wilson.l20010327@gmail.com>
2024-05-05 21:15:01 +0800

    Add the layout of the personal page
    Use the flatlist show in the personal page and each of them can navigate
    to the pet page the icon and the title should be modify in order to
    match the original layout

--- screen/Personal.js
+++ screen/Personal.js
@@ -1,9 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, FlatList, TouchableOpacity } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Header from '../component/Header-self';
 import PetPage from '../component/pet/Pet';
 const Stack = createNativeStackNavigator();
+const List = [
+  {
+    id: 1,
+    title: 'First Item',
+  },
+  {
+    id: 2,
+    title: 'Second Item',
+  },
+  {
+    id: 3,
+    title: 'Third Item',
+  },
+  {
+    id: 4,
+    title: 'Four Item',
+  },
+  {
+    id: 5,
+    title: 'Five Item',
+  },
+];
 export default function PersonalPage() {
   return (
     <Stack.Navigator>
@@ -20,13 +42,40 @@ function Main({ navigation }) {
   return (
     <View style={styles.container}>
       <Header />
-      <Text>This is the Personal page</Text>
-      <Button title='test'
+      <FlatList
+        style={styles.flat_container}
+        data={List}
+        renderItem={({ item }) => <Simple_element item={item} navigation={navigation} />}
+        keyExtractor={item => item.id}
+      />
+      {/* <Button title='test'
         onPress={() =>
           navigation.navigate('PetHome')
-        } />
+        } /> */}
       <StatusBar style="auto" />
-    </View>
+    </View >
+  )
+}
+function Simple_element({ item, navigation }) {
+  return (
+    <TouchableOpacity style={styles.element_outline}
+      onPress={() =>
+        navigation.navigate('PetHome')
+      }>
+      <View style={styles.element}>
+        <Image style={{
+          width: 50,
+          height: 50,
+          resizeMode: 'contain',
+        }} source={require('../assets/home-unclicked.png')} />
+        <View style={styles.word}>
+          <Text style={styles.innerText}>{item.title}</Text>
+        </View>
+        <View style={styles.next}>
+          <Text style={styles.innerText}>{'>'}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   )
 }
 const styles = StyleSheet.create({
@@ -34,6 +83,41 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-
+  },
+  flat_container: {
+    width: '100%',
+  },
+  element_outline: {
+    width: '100%',
+    height: 70,
+    // backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  element: {
+    width: '80%',
+    height: 70,
+    // backgroundColor: 'green',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  word: {
+    left: 10,
+    width: '60%',
+    height: 70,
+    justifyContent: 'center',
+    alignItems: 'left',
+  },
+  next: {
+    width: 30,
+    height: 30,
+    // backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'left',
+  },
+  innerText: {
+    color: 'black',
+    fontSize: 20
   },
 });
