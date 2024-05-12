63ce90c63e75d60687aebfbf455661b4b60f6820
Patrick Lin <patrick41513@gmail.com>
2024-05-12 17:17:02 +0800

    Add ai-assistant component to each page

--- screen/Trip.js
+++ screen/Trip.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Image, View } from 'react-native';
-import Header from '../component/Header-self';
+import React, { useContext } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import Header from "../component/Header-self";
+import MoveableButton from "../component/button/MovableButton";
+import ChatRoom from "../component/ChatRoom";
+import { AppStateContext } from "../AppStateContext";
+import RedirectPage from "../component/RedirectPage";
 export default function TripPage() {
+  const [isChatVisible, setIsChatVisible] = React.useState(false);
+  const [isRedirectVisible, setIsRedirectVisible] = React.useState(false);
+  const { position, setPosition } = useContext(AppStateContext);
+
+  const handleChatToggle = () => {
+    setIsChatVisible(!isChatVisible);
+  };
+
+  const handleRedirectToggle = () => {
+    setIsChatVisible(false);
+    setIsRedirectVisible(!isRedirectVisible);
+  };
+
   return (
     <View style={styles.container}>
-      <Header/>
-      <Image style={styles.container} resizeMode='contain' source={require('../assets/trip.png')} />
+      {isRedirectVisible ? (
+        <RedirectPage
+          isVisible={isRedirectVisible}
+          onClose={handleRedirectToggle}
+        />
+      ) : (
+        <>
+          <Header />
+          <View style={{ zIndex: 1 }}>
+            <MoveableButton
+              onChatToggle={handleChatToggle}
+              position={position}
+              setPosition={setPosition}
+            />
+          </View>
+          <View style={{ zIndex: 2 }}>
+            <ChatRoom
+              isVisible={isChatVisible}
+              onClose={handleChatToggle}
+              onRedirectToggle={handleRedirectToggle}
+            />
+          </View>
+          <Image
+            style={styles.backgroundImage}
+            resizeMode="contain"
+            source={require("../assets/trip.png")}
+          />
+        </>
+      )}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +59,15 @@ export default function TripPage() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  backgroundImage: {
+    position: "absolute",
+    top: 60,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    zIndex: -1, 
   },
 });
